5cab9354308c6f41ca60926330c091ad11447545
ErickTrujilloOsorio <ericktrujillo011@gmail.com>
2025-11-27 20:12:42 -0600

    fix

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ use Config\Paths;
  *---------------------------------------------------------------
  */
 
-$minPhpVersion = '8.1'; // If you update this, don't forget to update `spark`.
+$minPhpVersion = '8.1';
 if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
     $message = sprintf(
         'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
@@ -41,19 +41,12 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
  *---------------------------------------------------------------
  * BOOTSTRAP THE APPLICATION
  *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
  */
 
-// LOAD OUR PATHS CONFIG FILE
-// This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
+// Correct path to Paths.php for Azure (CI in root)
+$pathsPath = realpath(__DIR__ . '/app/Config/Paths.php');
+require $pathsPath;   // ← ESTA LÍNEA ES LA QUE TE FALTABA
 
 $paths = new Paths();
 
-// LOAD THE FRAMEWORK BOOTSTRAP FILE
-require $paths->systemDirectory . '/Boot.php';
-
-exit(Boot::bootWeb($paths));
+// LOAD THE FRAMEWORK
